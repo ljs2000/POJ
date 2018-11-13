a4a0fb7661d314760959c96e591fb04a8dd04dd5
Lucas <lucasserafin21@gmail.com>
2018-11-13 08:27:39 -0500

    Created Main.java
    Placeholder for the Eclipse Workspace Folder

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+* Hello World Program
+* Created By Lucas Serafin
+* Pd. 1
+* 11/12/18
+*/
+
+public class Main { 
+
+public static void main(String[] args) { 
+
+}
+
+System.out.println("Hello World!");
+
+}

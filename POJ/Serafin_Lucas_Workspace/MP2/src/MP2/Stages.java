9cf9a6a6523376631af721537a5a8b56b0a4e32b
Lucas Serafin <lucasserafin21@gmail.com>
2018-11-16 08:13:30 -0500

    Add files via upload

---
+++ POJ/Serafin_Lucas_Workspace/MP2/src/MP2/Stages.java
@@ -0,0 +1,22 @@
+package MP2;
+
+import java.util.Scanner;
+
+public class Stages {
+	
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+		int age;
+		System.out.println("Enter your age: \n");
+		age = input.nextInt();
+		if(age > 18) {
+		System.out.println("You are an adult. \n");
+		}
+		else if(age <= 18) {
+			System.out.println("You are a teenager. \n");
+		}
+		else if(age <=12) {
+			
+		}
+		}
+	}
---
+++ POJ/Serafin_Lucas_Workspace/MP2/src/MP2/SurfsUp2.java
@@ -0,0 +1,27 @@
+package MP2;
+
+import java.util.Scanner;
+
+public class SurfsUp2 {
+	
+	public static void main(String[] args) {
+		
+	int waveHeight;
+	final int wave = 6;
+		
+	Scanner reader = new Scanner(System.in);
+	System.out.println("Enter a height of the wave: \n");
+	waveHeight = reader.nextInt();
+	if(waveHeight >= wave) {
+		System.out.println("Great day for surfing! \n");
+		
+	}
+	
+	else {
+		
+		System.out.println("Go body boarding!");
+		
+	}
+		
+	}
+}

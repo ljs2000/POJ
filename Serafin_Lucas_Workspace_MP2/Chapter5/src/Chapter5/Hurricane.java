ba972874483ba2f3d2b38cf1d1186552fcc07426
Lucas Serafin <lucasserafin21@gmail.com>
2018-12-04 08:16:23 -0500

    Add files via upload

---
+++ Serafin_Lucas_Workspace_MP2/Chapter5/src/Chapter5/Hurricane.java
@@ -0,0 +1,27 @@
+package Chapter5;
+
+import java.util.Scanner;
+
+public class Hurricane {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	int category1;
+	int category2;
+	int category3;
+	int category4;
+	int category5;
+	int var;
+	System.out.println("Enter a category: ");
+	var = input.nextInt();
+	
+	switch(var) {
+	case 74:
+	case 75:
+	case 76:
+	case 77:
+		
+	
+	
+	}
+	}
+}

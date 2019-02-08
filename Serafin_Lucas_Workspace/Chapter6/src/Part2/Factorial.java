9797607156b9710f491344c6c0f9ce96a181a4a1
Lucas Serafin <lucasserafin21@gmail.com>
2019-02-08 07:55:36 -0500

    added most recent work space

---
+++ Serafin_Lucas_Workspace/Chapter6/src/Part2/Factorial.java
@@ -0,0 +1,22 @@
+package Part2;
+import java.util.Scanner;
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number = 1;
+		int factorial = 1;
+		System.out.println("Enter a number: \n");
+		number = input.nextInt();		
+		
+		for(int i = number; number < 1; number--) {
+			factorial *= number;
+		}
+			
+		System.out.println();
+		
+		input.close();	
+	}
+
+}
---
+++ Serafin_Lucas_Workspace/Chapter6/src/Part2/Oddsum.java
@@ -0,0 +1,15 @@
+package Part2;
+import java.util.Scanner;
+
+public class Oddsum {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number;
+		System.out.println("Enter a number: \n");
+		number = input.nextInt();
+
+		input.close();	
+	}
+
+}

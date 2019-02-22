604736ff4b8524aa001f1a9a396e75dcfc39355f
Lucas Serafin <lucasserafin21@gmail.com>
2019-02-22 07:45:05 -0500

    Update AccountSetup.java

--- Serafin_Lucas_Workspace/Chapter6/src/Part2/AccountSetup.java
+++ Serafin_Lucas_Workspace/Chapter6/src/Part2/AccountSetup.java
@@ -4,14 +4,25 @@ import java.util.Scanner;
 
 public class AccountSetup {
 	
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		String username = NULL;
 		
-		String password = NULL;
+		String username = null;
+		
+		String password = null;
 		
 		System.out.println("Enter your username: \n");
-		username = input.nextInt();
-	}
+		username = input.next();
+		
+		for(String i = password; password.length() <= 8;) {
+			
+			System.out.println("Enter your password that is at least 8 characters: \n");
+			password = input.toString();
+			
+		}
+	
 
+	
+}
+	
 }

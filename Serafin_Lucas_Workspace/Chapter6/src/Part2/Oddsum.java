5e197f30740dd7535ad026f10852d8e251469705
Lucas Serafin <lucasserafin21@gmail.com>
2019-02-22 07:46:02 -0500

    Update Oddsum.java

--- Serafin_Lucas_Workspace/Chapter6/src/Part2/Oddsum.java
+++ Serafin_Lucas_Workspace/Chapter6/src/Part2/Oddsum.java
@@ -8,7 +8,11 @@ public class Oddsum {
 		int number;
 		System.out.println("Enter a number: \n");
 		number = input.nextInt();
-
+		int sum = 0;
+		for(int i = 1; i <= number; i+=2) {
+			sum += i;
+		}
+		System.out.println("The sum of the odd number entered is: \n" + sum);
 		input.close();	
 	}
 

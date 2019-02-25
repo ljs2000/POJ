dbeb227bc7fdeb570a968789975f36ef43058e37
Lucas Serafin <lucasserafin21@gmail.com>
2019-02-25 08:14:38 -0500

    Add files via upload

---
+++ Serafin_Lucas_Workspace/Chapter6/src/Part2/powersTable.java
@@ -0,0 +1,15 @@
+package Part2;
+
+public class powersTable {
+	public static void main(String[] args) {
+		int number = 0;
+		int sum = 0;
+		int x = 0;
+		int y = 0;
+		System.out.format("%10s, %8s, %8s, %8s, %8s", "x^1", "x^2", "x^3", "x^4", "x^5", "x^6");
+		// You are trying to output six numbers per one line. The first line is incrementing by x each time. Find what x is and then find a way to increment by that number six times.
+		for(int i = 1; number <= 6; (number)*x) {
+		}
+	}
+
+}
